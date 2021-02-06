835921976e444b9cf8791c2a23efb2729ea381d1
EC2 Default User <ec2-user@ip-172-31-87-84.ec2.internal>
2021-02-06 18:57:09 +0000

    I finished spotify.py, so now all that's left is to finish up app.py

--- spotify.py
+++ spotify.py
@@ -7,18 +7,23 @@ import random
 
 load_dotenv(find_dotenv())
 
+
+#This function is purely for retreiving the data. That's it. I hardcoded in 3 separate artist id's.
 def find_albums():
+    
+    #These are my artist id's. 
     jack_johnson_uri = 'spotify:artist:3GBPw9NK25X1Wt2OUvOwY3'
     heilung_uri = 'spotify:artist:7sTKZr30LqC928DZ5P9mNQ'
     ville_uri = 'spotify:artist:6Kvpu1Xs687rTFj4qlGg4h'
     
+    
+    #This is setting up to randomly choose one of the artists.
+    artists = [jack_johnson_uri,heilung_uri,ville_uri]
+    num = random.randint(0,2)
+    
+    
+    
     spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-    
-    results = spotify.artist_albums(jack_johnson_uri, album_type='album')
-    albums = results['items']
-    while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
-    
-    for album in albums:
-    print(album['name'])
+    #And here, the artist is randomly chosen and their data is returned to app.py
+    results = spotify.artist_top_tracks(artists[num])
+    return results
