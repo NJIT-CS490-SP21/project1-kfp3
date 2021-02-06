dddb80f8d60af09a22b7ebc8410893da78480d1e
EC2 Default User <ec2-user@ip-172-31-87-84.ec2.internal>
2021-02-06 18:36:55 +0000

    Used Spotipy library, created readme, and have added some basic code so far.

--- app.py
+++ app.py
@@ -2,3 +2,17 @@ import os
 from Flask import Flask, render_template
 import requests
 from dotenv import load_dotenv,find_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+app = Flask(__name__)
+
+@app.route('/')
+def display():
+    find_albums()
+    
+
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv('IP', '0.0.0.0')
+)
--- spotify.py
+++ spotify.py
@@ -1,6 +1,24 @@
 import os
-from Flask import Flask, render_template
 import requests
 from dotenv import load_dotenv,find_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import random
 
 load_dotenv(find_dotenv())
+
+def find_albums():
+    jack_johnson_uri = 'spotify:artist:3GBPw9NK25X1Wt2OUvOwY3'
+    heilung_uri = 'spotify:artist:7sTKZr30LqC928DZ5P9mNQ'
+    ville_uri = 'spotify:artist:6Kvpu1Xs687rTFj4qlGg4h'
+    
+    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    
+    results = spotify.artist_albums(jack_johnson_uri, album_type='album')
+    albums = results['items']
+    while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+    
+    for album in albums:
+    print(album['name'])
