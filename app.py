ac55e56b47cbfd6aea7c2efd9dc36f6ed8b3ab7b
EC2 Default User <ec2-user@ip-172-31-87-84.ec2.internal>
2021-02-06 19:28:16 +0000

    I've got app.py running as it should, and just have to get it passing data to index.html, which I've also initialized

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 import os
-from Flask import Flask, render_template
+from flask import Flask, render_template
 import requests
 from dotenv import load_dotenv,find_dotenv
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotify import *
+import random
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route('/')
+#@app.route('/')
 def display():
-    find_albums()
+    #Fetching artist's data.
+    lst = find_albums()
     
-
-app.run(
-    port=int(os.getenv('PORT', 8080)),
-    host=os.getenv('IP', '0.0.0.0')
-)
+    #Creating random number so the same song won't always be shown.
+    num=random.randint(0,9)
+    
+    #Pass data to index.html
+    track = lst['tracks'][num]
+    name='track    : ' + track['name']
+    artist_name = lst['tracks'][0]['album']['artists'][0]['name']
+    if track['preview_url'] is not None:
+        url= 'audio    : ' + track['preview_url']
+    else:
+        url='There is no preview url for this specific song.'
+    album_cover='cover art: ' + track['album']['images'][0]['url']
+    print()
+    #return render_template(
+     #   "index.html",
+      #  artists=lst,
+    #)
+if __name__ == '__main__':
+    display()
+#app.run(
+ #   port=int(os.getenv('PORT', 8080)),
+  #  host=os.getenv('IP', '0.0.0.0')
+#)
