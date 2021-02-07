e33dca785703ee99d0863e56b9ff4f454e1b6a60
EC2 Default User <ec2-user@ip-172-31-87-84.ec2.internal>
2021-02-07 19:53:28 +0000

    This is the finished product. I couldn't get an audio player for the preview url, but I can definitely try that for Milestone2

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv,find_dotenv
 from spotify import *
 import random
 
-#app = Flask(__name__)
+app = Flask(__name__)
+app.debug=True
 
-#@app.route('/')
+@app.route('/')
 def display():
     #Fetching artist's data.
     lst = find_albums()
@@ -23,24 +24,25 @@ def display():
     
     #Artist's name
     artist_name = lst['tracks'][0]['album']['artists'][0]['name']
-    
     #Check if song has preview url. Otherwise print something else.
     if track['preview_url'] is not None:
-        url= 'audio    : ' + track['preview_url']
+        url="audio:   " + track['preview_url']
     else:
         url='There is no preview url for this specific song.'
         
     #Album cover
-    album_cover='cover art: ' + track['album']['images'][0]['url']
-    
+    album_cover=track['album']['images'][0]['url']
     #This is where the data gets passed.
-    #return render_template(
-     #   "index.html",
-      #  artists=lst,
-    #)
-if __name__ == '__main__':
-    display()
-#app.run(
- #   port=int(os.getenv('PORT', 8080)),
-  #  host=os.getenv('IP', '0.0.0.0')
-#)
+    return render_template(
+        "index.html",
+        artist=artist_name,
+        track_name=name,
+        cover=album_cover,
+        track_preview=url
+    )
+
+
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv('IP', '0.0.0.0')
+)
