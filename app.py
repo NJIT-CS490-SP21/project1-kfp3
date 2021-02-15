6c958e786e5cbc19b72dbed7e1c0b2414d7c3bb3
ChemEtoSoftware <kevinpereira79@gmail.com>
2021-02-15 16:52:38 +0000

    Made a mistake on this file

--- app.py
+++ app.py
@@ -39,8 +39,9 @@ def display():
     album_cover=track['album']['images'][0]['url']
     #This is where the data gets passed.
     
-    if artist is None:
-        artist = "There was an error fetching the artist's name"
+    #error check
+    if artist_name is None:
+        artist_name = "There was an error fetching the artist's name"
     if name is None:
         name = "There was an error fetching the song name"
     if album_cover is None:
